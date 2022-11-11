16770aafcfe4914628319778b5226018fde1535b
fas3a <fas3a4real@gmail.com>
2022-11-11 11:31:29 +0000

    Added Slides Images

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-let slideIndex = 1
-showSlides(slideIndex)
+let slideIndex = 1;
+showSlides(slideIndex);
 
 // Next or Prev Control
 function fwdSlides(n) {
   showSlides(slideIndex += n)
 }
 
-function showSlides(n){
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("prof_img");
-  console.log(slides.length);
-  if(n > slides.length) {slideIndex = 1};
-  if(n < 1) {slideIndex = slides.length};
-
-  for(i=0; i<slides.lenght; i++){
-    slides[i].style.display = "none"
-  }
-  slides[slideIndex-1].style.display = "block"
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  } 
+  slides[slideIndex-1].style.display = "block";
 }
